d8c69b114f28ca1a9858a099a05e64bda1eac173
demossil demossil <demossil demossil>
2016-03-06 13:45:35 +0800

    matrix_inv

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_x <- NULL
+    setMatrix <- function(matrix){
+        x <<- matrix;
+        inv_x <<- NULL
+    }
+    getMatrix <- function() x
+    setInverse <- function(inv) inv_x <<- inv
+    getInverse <- function() inv_x
+    list(setMatrix = setMatrix, getMatrix = getMatrix, 
+         setInverse = setInverse, getInverse = getInverse)
+    
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv_x <- x$getInverse()
+    if(!is.null(inv_x)){
+        message("getting cached data")
+        return(inv_x)
+    }
+    matrix <- x$getMatrix()
+    inv_x <- solve(matrix)
+    x$setMatrix(inv_x)
+    inv_x
 }
